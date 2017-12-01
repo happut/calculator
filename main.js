0a5b33ee669a702aff17acf9c22d121e32d7fe6d
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-12-01 14:22:01 +0800

    add class content

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 var calcExports = require('./calc.js');
 
-console.log(calcExports.addFunc(1, 2));
+var v1obj = document.getElementById("v1");
+var v2obj = document.getElementById("v2");
+var btobj = document.getElementById("bt");
+var resobj = document.getElementById("res");
+
+btobj.onclick = function () {
+    res.value = (v1obj.value-0) + (v2obj.value-0);
+}
